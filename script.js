712572fc12aa3c2d2a51b023b96a9aefbe4dd026
Mdanny-1983 <dwallingre@gmail.com>
2026-03-22 11:21:06 -0300

    Add files via upload
    Repositorio Oficial de Gestión Digital - Club Atlético Basavilbaso
    Este espacio contiene el código fuente de la plataforma web del "Decano". Incluye la página principal institucional y el sistema local de registro de socios con generación de carnet en PDF. Desarrollado con tecnologías web estándar (HTML5, CSS3 y JavaScript) enfocadas en la rapidez y la accesibilidad para toda la comunidad de Basavilbaso.

---
+++ script.js
@@ -0,0 +1,10 @@
+﻿document.addEventListener("DOMContentLoaded", () => {
+    // Seleccionamos el botón dentro de .hero
+    const btnSocio = document.querySelector(".hero button");
+    
+    if (btnSocio) {
+        btnSocio.addEventListener("click", () => {
+            alert("¡Gracias por tu interés! Próximamente habilitaremos el sistema de socios online para el Club.");
+        });
+    }
+});
